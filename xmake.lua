cfd4e16b8a28ce2dc5d05f105447329294227122
GochenRyan <rangaocheng@outlook.com>
2023-04-13 18:20:36 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,4 @@
-target("libSDL")
+target("SDL3")
     add_includedirs(
         "$(projectdir)/Vendor/SDL/include/"
     )
@@ -18,23 +18,21 @@ target("libSDL")
 
     after_build("windows", function(target)
         os.cp("$(projectdir)/Vendor/SDL/build/Debug/*.lib", "$(projectdir)/lib/")
-        os.trymv("$(projectdir)/lib/SDL3d.lib", "$(projectdir)/lib/libSDL.lib")
-        os.trymv("$(projectdir)/lib/SDL3.lib", "$(projectdir)/lib/libSDL.lib")
+        os.trymv("$(projectdir)/lib/SDL3d.lib", "$(projectdir)/lib/SDL3.lib")
+        os.trymv("$(projectdir)/lib/SDL3.lib", "$(projectdir)/lib/SDL3.lib")
 
         os.cp("$(projectdir)/Vendor/SDL/build/Debug/*.dll", "$(projectdir)/lib/")
-        os.trymv("$(projectdir)/lib/SDL3d.dll", "$(projectdir)/lib/libSDL.dll")
-        os.trymv("$(projectdir)/lib/SDL3.dll", "$(projectdir)/lib/libSDL.dll")
+        os.trymv("$(projectdir)/lib/SDL3d.dll", "$(projectdir)/lib/SDL3.dll")
+        os.trymv("$(projectdir)/lib/SDL3.dll", "$(projectdir)/lib/SDL3.dll")
 
         os.cp("$(projectdir)/Vendor/SDL/build/Debug/*.pdb", "$(projectdir)/lib/")
-        os.trymv("$(projectdir)/lib/SDL3d.pdb", "$(projectdir)/lib/libSDL.pdb")
-        os.trymv("$(projectdir)/lib/SDL3.pdb", "$(projectdir)/lib/libSDL.pdb")
+        os.trymv("$(projectdir)/lib/SDL3d.pdb", "$(projectdir)/lib/SDL3.pdb")
+        os.trymv("$(projectdir)/lib/SDL3.pdb", "$(projectdir)/lib/SDL3.pdb")
     end)
 
     on_clean("windows", function(target)
-        os.cd("$(projectdir)/Vendor/SDL")
         if os.exists("$(buildir)") then
             os.rm("$(buildir)/")
         end
-        os.cd("$(projectdir)")
     end)
     
